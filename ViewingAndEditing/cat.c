46fbe56d44a7da2d701188d45bd9ea64c10cbf57
satishkumarpeddi <ayno58261@gmail.com>
2026-03-27 10:38:52 -0400

    Refactor file handling commands and remove unused main.c

--- ViewingAndEditing/cat.c
+++ ViewingAndEditing/cat.c
@@ -1,17 +1,117 @@
 #include <stdio.h>
-#include <fcntl.h>
-void main(){
-    int fd;
-    char buffer[100];
-    int n;
-    fd = open("text.txt",O_RDONLY);
-    if(fd<0){
-        perror("Error in opening.");
+#include <stdlib.h>
+
+void viewFile() {
+    char fname[100], ch;
+    FILE *fp;
+
+    printf("Enter file name to view: ");
+    scanf("%s", fname);
+
+    fp = fopen(fname, "r");
+    if (fp == NULL) {
+        printf("File not found!\n");
         return;
     }
-    while((n=read(fd,buffer,sizeof(buffer)))>0){
-        write(1,buffer,n);
+
+    printf("\n--- File Content ---\n");
+    while ((ch = fgetc(fp)) != EOF)
+        putchar(ch);
+
+    fclose(fp);
+}
+
+void editFile() {
+    char fname[100], ch;
+    FILE *fp;
+
+    printf("Enter file name to edit (append mode): ");
+    scanf("%s", fname);
+
+    fp = fopen(fname, "a");
+    if (fp == NULL) {
+        printf("Unable to open file!\n");
+        return;
     }
-    close(fd);
-    return;
+
+    printf("Enter text to append (press # to stop):\n");
+    while ((ch = getchar()) != '\n'); // clear buffer
+
+    while ((ch = getchar()) != '#')
+        fputc(ch, fp);
+
+    fclose(fp);
+}
+
+void copyFile() {
+    char src[100], dest[100], ch;
+    FILE *fs, *fd;
+
+    printf("Enter source file: ");
+    scanf("%s", src);
+    printf("Enter destination file: ");
+    scanf("%s", dest);
+
+    fs = fopen(src, "r");
+    if (fs == NULL) {
+        printf("Source file not found!\n");
+        return;
+    }
+
+    fd = fopen(dest, "w");
+    while ((ch = fgetc(fs)) != EOF)
+        fputc(ch, fd);
+
+    fclose(fs);
+    fclose(fd);
+
+    printf("File copied successfully.\n");
+}
+
+void moveFile() {
+    char src[100], dest[100];
+
+    printf("Enter source file: ");
+    scanf("%s", src);
+    printf("Enter destination file: ");
+    scanf("%s", dest);
+
+    if (rename(src, dest) == 0)
+        printf("File moved/renamed successfully.\n");
+    else
+        printf("Error moving file.\n");
+}
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n--- UNIX File Commands ---\n");
+        printf("1. View File (cat)\n");
+        printf("2. Edit File (append)\n");
+        printf("3. Copy File (cp)\n");
+        printf("4. Move/Rename File (mv)\n");
+        printf("5. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            viewFile();
+            break;
+        case 2:
+            editFile();
+            break;
+        case 3:
+            copyFile();
+            break;
+        case 4:
+            moveFile();
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("Invalid choice!\n");
+        }
+    }
 }
