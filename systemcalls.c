43c29f0a4ebc8ff020e43c510eb281fb2826998c
satishkumarpeddi <ayno58261@gmail.com>
2026-03-26 23:41:02 -0400

    Add implementation of file handling in C

---
+++ systemcalls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+void main(){
+    int fd;
+    fd = open("test.txt",O_CREAT|O_RDWR,0644);
+    if(fd<0){
+        printf("Error Opening file\n");
+        return;
+    }
+    printf("File Open successfully\n");
+    printf("File Descriptor: %d\n",fd);
+}
